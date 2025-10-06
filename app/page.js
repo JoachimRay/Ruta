ea0e63fe9a31200319d2deb54ca82f7fed19efa7
JoachimRay <131645680+JoachimRay@users.noreply.github.com>
2025-10-06 17:05:01 +0800

    Map Implementation

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
-export default function Page() {
+import CebuMap from "./components/CebuMap";
+
+
+export default function Home() {
   return (
     <html>
       <body>
         <h1>RUTA</h1>
+        <CebuMap/>
       </body>
     </html>
   );
